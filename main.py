3ce189da60bcecf4d2d697e257cceda0534bce18
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2026-01-07 11:13:11 +0100

    feat: visible from outside

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import argparse
+
 import uvicorn
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the Decrypto server")
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8000,
+        help="Port to run the server on (default: 8000)",
+    )
+
+    args = parser.parse_args()
+
     uvicorn.run(
-        "app:app",
-        host="127.0.0.1",
-        port=8000,
+        "app.main:app",
         reload=True,
+        host="0.0.0.0",  # if visible from the outside, else 127.0.0.1
+        port=args.port,
     )
